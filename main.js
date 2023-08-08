fd37156e1f570fc69c30584dd2f3f09a589d8450
Rebecca Nayala <140104442+rebeccanayala@users.noreply.github.com>
2023-08-08 10:03:48 -0300

    Add main.js

---
+++ main.js
@@ -0,0 +1,32 @@
+const form = document.getElementById('form-validation');
+
+let formValidity = false;
+
+function itemsValidity (valA, valB){
+    return valB > valA;
+}
+
+form.addEventListener('submit', function(e){
+    e.preventDefault();
+    let valA = parseFloat(document.getElementById('cost-price').value);
+    let valB = parseFloat(document.getElementById('sale-price').value);
+
+    const successmessage = 'Cadastro realizado com sucesso!';
+    const itemssuccessmessage = document.querySelector('.success-message');
+    
+    formValidity = itemsValidity(valA, valB);
+    
+    if (formValidity){
+        document.querySelector('.error-message').style.display = 'none';
+        itemssuccessmessage.innerHTML = successmessage;
+        itemssuccessmessage.style.display = 'block';
+        document.querySelector('.error-message').style.display = 'none';
+
+        valA.value = '';
+        valB.value = '';
+
+    } else {
+        document.querySelector('.error-message').style.display = 'block';
+        itemssuccessmessage.style.display = 'none';
+    }    
+})
