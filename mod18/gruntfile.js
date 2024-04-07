ab03a3e2cf493bb7498429a99653a173e38eba01
Rebecca Nayala <140104442+rebeccanayala@users.noreply.github.com>
2024-04-07 05:15:26 -0300

    Starting project

--- mod18/gruntfile.js
+++ mod18/gruntfile.js
@@ -6,7 +6,7 @@ module.exports = function(grunt) {
         less: {
             development: {
                 files: {
-                    'main.css': 'main.less'
+                    'dev/styles/main.css': 'src/styles/main.less'
                 }
             }, 
             production: {
@@ -14,34 +14,81 @@ module.exports = function(grunt) {
                     compress: true, 
                 }, 
                 files: {
-                    'main.min.css': 'main.less'
+                    'dist/styles/main.min.css': 'src/styles/main.less'
                 }
             }
         },
-        sass: {
+        watch: {
+            less: {
+                files: ['src/styles/**/*.less'], 
+                tasks: ['less:development']
+            },
+            html: {
+                files: ['src/index.html'],
+                tasks: ['replace:dev']
+            }
+        }, 
+        replace: {
+            dev: {
+                options: {
+                    patterns: [
+                        {
+                            match: 'ENDERECO_DO_CSS',
+                            replacement:'./styles/main.css'
+                        }
+                    ]
+                },
+                files: [
+                    {
+                        expand: true, 
+                        flatten: true, 
+                        src: ['src/index.html'],
+                        dest: 'dev/'
+                    }
+                ]
+            },
             dist: {
                 options: {
-                    style: 'compressed'
+                    patterns: [
+                        {
+                            match: 'ENDERECO_DO_CSS',
+                            replacement:'./styles/main.min.css'
+                        }
+                    ]
+                },
+                files: [
+                    {
+                        expand: true, 
+                        flatten: true, 
+                        src: ['prebuild/index.html'],
+                        dest: 'dist/'
+                    }
+                ]
+            }
+        },
+        htmlmin: {
+            dist: {
+                options: {
+                    removeComments: true, 
+                    collapseWhitespace: true
                 },
                 files: {
-                    'style.css': 'main.scss'
+                    'prebuild/index.html': 'src/index.html'
                 }
             }
-        }
-    });
-
-    grunt.registerTask('olaGrunt', function() {
-        const done = this.async();
-        setTimeout(function() {
-            console.log('Olá Grunt');
-            done();
-        },3000);
+        },
+        clean: ['prebuild']
     });
 
     grunt.loadNpmTasks('grunt-contrib-less');
-    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-replace');
+    grunt.loadNpmTasks('grunt-contrib-htmlmin');
+    grunt.loadNpmTasks('grunt-contrib-clean');
 
-    grunt.registerTask('default', ['less', 'sass']);
+    grunt.registerTask('default', ['watch']);
+    grunt.registerTask('build', ['less:production', 'htmlmin:dist', 'replace:dist', 'clean']);
+
 };
 
 
