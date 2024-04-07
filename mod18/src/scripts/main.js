91ebae401e42f2c7c2e471056939401f4a45b4ad
Rebecca Nayala <140104442+rebeccanayala@users.noreply.github.com>
2024-04-07 17:23:42 -0300

    Compressing JavaScript with Grunt

--- mod18/src/scripts/main.js
+++ mod18/src/scripts/main.js
@@ -1,3 +1,12 @@
 document.addEventListener('DOMContentLoaded', function() {
-    
+    document.getElementById('form-sorteador').addEventListener('submit', function(evento) {
+        evento.preventDefault();
+        let numeroMaximo = document.getElementById('numero-maximo').value;
+        numeroMaximo = parseInt(numeroMaximo);
+
+        let numeroAleatorio = Math.floor(Math.random() * numeroMaximo) + 1;
+
+        document.getElementById('resultado-valor').innerText = numeroAleatorio;
+        document.querySelector('.resultado').style.display = 'block';
+    })
 })
